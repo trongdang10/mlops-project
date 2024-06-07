62aeb3dcf9055d069b58a40da963bd53636ac538
trongdang10 <viettrong1010@gmail.com>
2024-06-07 09:53:50 +0200

    data ingestion & validation added

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-from mlProject import logger
+from src.mlProject import logger
+from src.mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("test logging")
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
