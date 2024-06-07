06bffef042a5fce009ad99f1be291eaeb5a22c96
trongdang10 <viettrong1010@gmail.com>
2024-06-07 14:02:22 +0200

    app added

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+import os 
+import numpy as np
+import pandas as pd
+from mlProject.pipeline.prediction import PredictionPipeline
+
+
+app = Flask(__name__) # initializing a flask app
+
+@app.route('/',methods=['GET'])  # route to display the home page
+def homePage():
+    return render_template("index.html")
+
+
+
+if __name__ == "__main__":
+	# app.run(host="0.0.0.0", port = 8080, debug=True)
+	app.run(host="0.0.0.0", port = 8080)
+      
+
